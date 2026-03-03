1b6a024c506ce75e5f5eb568e30763a18332774a
dima <ologdm.job@gmail.com>
2026-03-03 21:35:32 +0100

    add compose navigation3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    // mandatory for compose navigation3
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -53,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // mandatory for compose navigation3
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 }
