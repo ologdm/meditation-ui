494f80e32176cfaf95a65adf1ccaa7f6eb9d947b
dima <ologdm.job@gmail.com>
2026-03-08 21:43:32 +0100

    update Feature data class with detail fields

--- app/src/main/java/com/example/meditationuiapp/Feature.kt
+++ app/src/main/java/com/example/meditationuiapp/Feature.kt
@@ -5,10 +5,18 @@ import androidx.compose.ui.graphics.Color
 
 data class Feature(
     val id: Int,
-    val title: String,
+    val title: String, // Sleep meditation
     @DrawableRes val iconId: Int,
     // 3 colori per fare l'immagine custom
     val lightColor: Color,
     val mediumColor: Color,
-    val darkColor: Color
+    val darkColor: Color,
+
+    // detail category
+    val subtitle: String, // Best practice meditations
+    val category : String, // Sleep music
+    val time : String, // 45 min
+    val description: String, // Ease the mind into a restful night's sleep with these deep, amblent tones
+    val savedQty : Int, // 12,542
+    val listenQty : Int, // 43,453
 )
