e85c003c0b0fa2171edea4603bbe5761f4e6146f
dima <ologdm.job@gmail.com>
2026-03-08 16:50:30 +0100

    fix modifier top support navigationBarsPadding + systemBarsPadding

--- app/src/main/java/com/example/meditationuiapp/NavigationRoot.kt
+++ app/src/main/java/com/example/meditationuiapp/NavigationRoot.kt
@@ -23,11 +23,13 @@ fun NavigationRoot(
     NavDisplay(
         backStack = backStack,
         onBack = {
-            if (backStack.size > 1) { backStack.removeLastOrNull() } // TODO !! doesen't work, click back still exits from app
+            if (backStack.size > 1) {
+                backStack.removeLastOrNull()
+            } // TODO !! doesen't work, click back still exits from app
         },
         entryDecorators = listOf(
             rememberSaveableStateHolderNavEntryDecorator(), // mantiene lo stato dei composable tra navigazioni
-            rememberViewModelStoreNavEntryDecorator () // senza il viewmodel non ha scope l'entry ma l'activity
+            rememberViewModelStoreNavEntryDecorator() // senza il viewmodel non ha scope l'entry ma l'activity
         ),
         entryProvider = entryProvider {
             entry<Root.HomePage> { navKey ->
@@ -38,7 +40,10 @@ fun NavigationRoot(
 
             entry<Root.FeatureDetail> { navKey ->
                 // TODO: ok
-                FeatureDetailScreen(navKey.id) // passa id al viewmodel Detail
+                FeatureDetailScreen(
+                    modifier = modifier,
+                    id = navKey.id
+                ) // passa id al viewmodel Detail
             }
         }
     )
