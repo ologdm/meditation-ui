20241cd2a6cc50ef539c2fc1bce640afa2c9e9df
dima <ologdm.job@gmail.com>
2026-03-09 02:02:30 +0100

    implement scaffold in main activity for status bar + gesture navigation bar

--- app/src/main/java/com/example/meditationuiapp/MainActivity.kt
+++ app/src/main/java/com/example/meditationuiapp/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.navigation3.ui.NavDisplay
@@ -25,19 +29,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MeditationUiAppTheme {
-                NavigationRoot(
-                    modifier = Modifier
-                        .navigationBarsPadding()
-                        .systemBarsPadding()
-                        .fillMaxSize(),
-                )
-//                HomeScreen(
-//                    // Aggiunge solo il padding necessario
-//                    modifier = Modifier
-//                        .navigationBarsPadding()
-//                        .systemBarsPadding(),
-//                    onFeatureClick = {}
-//                )
+                Scaffold(
+                    containerColor = DeepBlue
+                ) { padding ->
+                    NavigationRoot(
+                        modifier = Modifier
+                            .padding(padding)
+                            .fillMaxSize()
+                    )
+                }
             }
         }
     }
